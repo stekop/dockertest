18c99df7fb95b8f73049803292ab14ff2080a6d5
stekop <mail@stefankopp.com>
2016-08-19 22:50:26 +0200

    Initial

---
+++ src/main/java/de/flowingminds/DockertestApplication.java
@@ -0,0 +1,12 @@
+package de.flowingminds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockertestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockertestApplication.class, args);
+	}
+}
---
+++ src/test/java/de/flowingminds/DockertestApplicationTests.java
@@ -0,0 +1,16 @@
+package de.flowingminds;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DockertestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
